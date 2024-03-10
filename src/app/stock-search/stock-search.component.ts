11596ed6379d9189f4cc9d17b5b481bc1fa9a642
Vedanth Subramaniam <vedanth.1113@gmail.com>
2024-03-09 21:46:47 -0800

    timeout apis init

--- src/app/stock-search/stock-search.component.ts
+++ src/app/stock-search/stock-search.component.ts
@@ -1,8 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 import {StockApiService} from "../stock-api.service";
-
+import {Subscription, interval, switchMap, forkJoin} from 'rxjs';
 @Component({
   selector: 'app-stock-search',
   standalone: true,
@@ -12,9 +12,10 @@ import {StockApiService} from "../stock-api.service";
   templateUrl: './stock-search.component.html',
   styleUrl: './stock-search.component.css'
 })
-export class StockSearchComponent implements OnInit {
+export class StockSearchComponent implements OnInit, OnDestroy {
 
   tickerSymbol: string = '';
+  private apiSubscription: Subscription | undefined;
 
   constructor(private stockService: StockApiService) {
   }
@@ -26,34 +27,37 @@ export class StockSearchComponent implements OnInit {
   searchStock() {
     console.log('Searching for stock:', this.tickerSymbol);
 
-    this.stockService.getCompanyCommonDetailsAPI(this.tickerSymbol).subscribe({
-      next: (response: any) => {
-        console.log(response);
-      }
-    });
-
-    this.stockService.getNewsTabDetailsAPI(this.tickerSymbol).subscribe({
-      next: (response: any) => {
-        console.log(response);
-      }
-    });
-
-    this.stockService.getChartsTabDetailsAPI(this.tickerSymbol).subscribe({
-      next: (response: any) => {
-        console.log(response);
-      }
-    });
-
-    this.stockService.getInsightsTabDetailsAPI(this.tickerSymbol).subscribe({
-      next: (response: any) => {
-        console.log(response);
-      }
-    });
-
+    this.apiSubscription = interval(15000)
+      .pipe(
+        switchMap(() => forkJoin({
+          companyCommonDetails: this.stockService.getCompanyCommonDetailsAPI(this.tickerSymbol),
+          newsTabDetails: this.stockService.getNewsTabDetailsAPI(this.tickerSymbol),
+          chartsTabDetails: this.stockService.getChartsTabDetailsAPI(this.tickerSymbol),
+          insightsTabDetails: this.stockService.getInsightsTabDetailsAPI(this.tickerSymbol)
+        }))
+      )
+      .subscribe({
+        next: (responses) => {
+          console.log('Company Common Details:', responses.companyCommonDetails);
+          console.log('News Tab Details:', responses.newsTabDetails);
+          console.log('Charts Tab Details:', responses.chartsTabDetails);
+          console.log('Insights Tab Details:', responses.insightsTabDetails);
+        },
+        error: (error) => {
+          console.error('Error in API calls:', error);
+        }
+      });
   }
 
   clearSearch() {
     this.tickerSymbol = '';
     // Implement the logic to clear out the currently searched  results and show the initial search page
+  }
+
+  ngOnDestroy() {
+    console.log("Bye");
+    if (this.apiSubscription) {
+      this.apiSubscription.unsubscribe();
+    }
   }
 }
